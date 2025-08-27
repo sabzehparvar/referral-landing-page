52f4f52e1856c650a2b8c12bd8780ab8ad47efa5
mojtaba <sabzehparvar.mojtaba@gmail.com>
2025-08-27 15:46:58 +0330

    changed app download btn css, changed carousel desc colors, fixed faq FOUC issue on loading

--- Referral/dist/js/index.js
+++ Referral/dist/js/index.js
@@ -1,37 +1,45 @@
-const player = new Plyr('#Player', {
-    controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen']
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const player = new Plyr("#Player", {
+    controls: [
+      "play-large",
+      "play",
+      "progress",
+      "current-time",
+      "mute",
+      "volume",
+      "fullscreen",
+    ],
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const btn = document.querySelector('#FaqToggleBtn button');
-    const btnText = document.querySelector('#FaqToggleBtn button span');
-    const arrow = document.querySelector('#FaqToggleBtn button .faq-arrow');
-    const faqList = document.getElementById('FaqAccordion');
-    const extras = faqList.querySelectorAll('ul > li:nth-child(n+4)');
+  const btn = document.querySelector("#FaqToggleBtn button");
+  const btnText = document.querySelector("#FaqToggleBtn button span");
+  const arrow = document.querySelector("#FaqToggleBtn button .faq-arrow");
+  const faqList = document.getElementById("FaqAccordion");
+  const extras = faqList.querySelectorAll("ul > li:nth-child(n+4)");
 
-    if (!extras.length) {
-        btn.classList.add('uk-hidden')
-        return
-    }
-    let expanded = false;
+  if (!extras.length) {
+    btn.classList.add("uk-hidden");
+    return;
+  }
+  let expanded = false;
 
-    extras.forEach(li => {
-        li.classList.add('faq-collapsed');
-        li.classList.remove('faq-expanded');
+  extras.forEach((li) => {
+    li.classList.add("faq-collapsed");
+    li.classList.remove("faq-expanded");
+  });
+
+  btn.addEventListener("click", function () {
+    expanded = !expanded;
+    extras.forEach((li) => {
+      if (expanded) {
+        li.classList.remove("faq-collapsed");
+        li.classList.add("faq-expanded");
+      } else {
+        li.classList.add("faq-collapsed");
+        li.classList.remove("faq-expanded");
+      }
     });
-
-    btn.addEventListener('click', function () {
-        expanded = !expanded;
-        extras.forEach(li => {
-            if (expanded) {
-                li.classList.remove('faq-collapsed');
-                li.classList.add('faq-expanded');
-            } else {
-                li.classList.add('faq-collapsed');
-                li.classList.remove('faq-expanded');
-            }
-        });
-        btnText.textContent = expanded ? 'نمایش کمتر' : 'نمایش بیشتر';
-        arrow.classList.toggle('rotated', expanded);
-    });
+    btnText.textContent = expanded ? "نمایش کمتر" : "نمایش بیشتر";
+    arrow.classList.toggle("rotated", expanded);
+  });
 });
